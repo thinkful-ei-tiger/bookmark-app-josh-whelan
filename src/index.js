b419df273ef56af2f62956a22e3b438dc096d0b1
Joshua Whelan <josh25whelan@gmail.com>
2020-10-28 16:28:56 -0400

    style update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import $ from 'jquery'
 
+import './index.css'
+
+
 function main() {
   console.log('DOM is loaded')
 
-  const startMsg = $('<p>Webpack is working!</p>')
-  $('#root').append(startMsg)
 }
 
 $(main)
